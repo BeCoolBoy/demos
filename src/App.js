006143998c6c2bd041a4855da860101f52257492
anjalifromkpm <mshankar803506@gmail.com>
2022-07-16 19:22:24 +0530

    recaptcha

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import './App.css';
 function App() {
-  function onSubmit(token) {
-    document.getElementById("demo-form").submit();
-  }
   return (
     <>
       <form id="form_id" method="post" action="your_action.php">
-          <input type="hidden" id="g-recaptcha-response" name="g-recaptcha-response" />
-          <input type="hidden" name="action" value="validate_captcha"/>
+        <input type="text" name="text" />
+        <div
+            class="g-recaptcha"
+            data-sitekey="6LeIxAcTAAAAAJcZVRqyHh71UMIEGNQ_MXjiZKhI"
+            data-callback="onRecaptchaSuccess"
+            data-expired-callback="onRecaptchaResponseExpiry"
+            data-error-callback="onRecaptchaError"
+          >
+        </div>
+        <input type="submit" value="submit" />
       </form>
     </>
   );
