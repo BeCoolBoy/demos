9d111945979708f2855972cf6f3c99162a0d24f2
anjalifromkpm <mshankar803506@gmail.com>
2022-07-16 18:46:54 +0530

    recaptcha

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
       <form style={{margin:"10px"}} method="POST" action='https://developers.google.com/recaptcha/intro'>
         <input type="text" name="name" required placeholder='name'/><br /><br />
         <input type="number" name="number" required placeholder='number'/><br /><br />
-        <div class="g-recaptcha" data-sitekey="your_site_key" ></div>
+        <div class="g-recaptcha" data-sitekey="6Lcy3PUgAAAAABK8VPU-U-eI4zTqlaM7BDb5_g38" ></div>
         <input type="submit" value="submit" />
       </form>
     </>
