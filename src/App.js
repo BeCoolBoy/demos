30f45d0dfbffbd6e1265330e8451a4e3183a0bdc
anjalifromkpm <mshankar803506@gmail.com>
2022-07-16 18:30:05 +0530

    hi

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
       <form style={{margin:"10px"}}>
         <input type="text" name="name" required placeholder='name'/><br /><br />
         <input type="number" name="number" required placeholder='number'/><br /><br />
-        <div class="g-recaptcha" data-sitekey="your_site_key"></div>
+        <div class="g-recaptcha" data-sitekey="your_site_key" ></div>
         <input type="submit" value="submit" />
       </form>
     </>
