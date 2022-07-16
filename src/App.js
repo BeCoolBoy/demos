7a35243e733281b134643baea43fce7f46a1996f
anjalifromkpm <mshankar803506@gmail.com>
2022-07-16 19:10:32 +0530

    recaptcha

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import './App.css';
 function App() {
+  function onSubmit(token) {
+    document.getElementById("demo-form").submit();
+  }
   return (
     <>
-      <form style={{margin:"10px"}} method="POST" action='https://developers.google.com/recaptcha/intro'>
-        <input type="text" name="name" required placeholder='name'/><br /><br />
-        <input type="number" name="number" required placeholder='number'/><br /><br />
-        <div class="g-recaptcha" data-sitekey="6Lcy3PUgAAAAABK8VPU-U-eI4zTqlaM7BDb5_g38" ></div>
-        <input type="submit" value="submit" />
+      <form id="form_id" method="post" action="your_action.php">
+          <input type="hidden" id="g-recaptcha-response" name="g-recaptcha-response" />
+          <input type="hidden" name="action" value="validate_captcha"/>
       </form>
     </>
   );
