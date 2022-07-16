97c3d4cfa9627094050b28ffc2db5d7a34eb308e
anjalifromkpm <mshankar803506@gmail.com>
2022-07-16 19:50:21 +0530

    recaptcha

---
+++ src/App copy 2.js
@@ -0,0 +1,27 @@
+import './App.css';
+function App() {
+ 
+  return (
+    <>
+      {/* https://dev.to/huzaifa99/add-google-recaptcha-v2-to-plain-html-3n0p */}
+      <form id="form_id" method="post" action="your_action.php">
+        <input type="text" name="text" />
+        <div 
+        className="g-recaptcha" 
+        data-sitekey="6LeIxAcTAAAAAJcZVRqyHh71UMIEGNQ_MXjiZKhI" 
+        data-callback="correctCaptcha"></div>
+        {/* <div
+            class="g-recaptcha"
+            data-sitekey="6LeIxAcTAAAAAJcZVRqyHh71UMIEGNQ_MXjiZKhI"
+            data-callback="onRecaptchaSuccess"
+            data-expired-callback="onRecaptchaResponseExpiry"
+            data-error-callback="onRecaptchaError"
+          >
+        </div> */}
+        <input type="submit" value="submit" />
+      </form>
+    </>
+  );
+}
+
+export default App;
--- src/App.js
+++ src/App.js
@@ -1,18 +1,12 @@
 import './App.css';
 function App() {
+ 
   return (
     <>
-      <form id="form_id" method="post" action="your_action.php">
-        <input type="text" name="text" />
-        <div
-            class="g-recaptcha"
-            data-sitekey="6LeIxAcTAAAAAJcZVRqyHh71UMIEGNQ_MXjiZKhI"
-            data-callback="onRecaptchaSuccess"
-            data-expired-callback="onRecaptchaResponseExpiry"
-            data-error-callback="onRecaptchaError"
-          >
-        </div>
-        <input type="submit" value="submit" />
+      <form method="post" onSubmit="return submitUserForm()">
+        <div className="g-recaptcha" data-sitekey="6Lfa6vUgAAAAAKSGgEiAS85Kc4pTrc8Us__4tdJI" data-callback="verifyCaptcha"></div>
+        <div id="g-recaptcha-error"></div>
+        <input type="submit" name="submit" value="Submit" />
       </form>
     </>
   );
