56351c143890a1b34ecc3f12434863459ca9ca93
anjalifromkpm <mshankar803506@gmail.com>
2022-07-16 18:38:38 +0530

    recaptcha

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import './App.css';
 function App() {
   return (
     <>
-      <form style={{margin:"10px"}}>
+      <form style={{margin:"10px"}} method="POST" action='https://developers.google.com/recaptcha/intro'>
         <input type="text" name="name" required placeholder='name'/><br /><br />
         <input type="number" name="number" required placeholder='number'/><br /><br />
         <div class="g-recaptcha" data-sitekey="your_site_key" ></div>
