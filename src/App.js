e399b7d5ec109857d22ee5de7523f9aecfc9b902
anjalifromkpm <mshankar803506@gmail.com>
2022-07-17 13:53:10 +0530

    recaptcha

--- src/App.js
+++ src/App.js
@@ -1,13 +1,45 @@
 import './App.css';
 function App() {
- 
+//  https://artisansweb.net/validate-google-recaptcha-using-javascript/
+// action='https://artisansweb.net/validate-google-recaptcha-using-javascript/'
+      var recaptcha_response = '';
+      function submitUserForm() {
+          if(recaptcha_response==''){
+            console.log("please select roboto")
+            return false;
+          }
+          return true;
+          // if(recaptcha_response.length > 0) {
+          //     document.getElementById('g-recaptcha-error').innerHTML = '<span style="color:red;">This field is required.</span>';
+          //     return false;
+          // }
+      }
+      
+      function verifyCaptcha(token) {
+          recaptcha_response = token;
+          // document.getElementById('g-recaptcha-error').innerHTML = '';
+      }
   return (
     <>
-      <form method="post" onSubmit="return submitUserForm()">
+      {/* <form  onSubmit="return submitUserForm(this)" method="post">
         <div className="g-recaptcha" data-sitekey="6Lfa6vUgAAAAAKSGgEiAS85Kc4pTrc8Us__4tdJI" data-callback="verifyCaptcha"></div>
         <div id="g-recaptcha-error"></div>
-        <input type="submit" name="submit" value="Submit" />
-      </form>
+        <input id="submit" type="submit" />
+      </form> */}
+      <form onSubmit="return submitUserForm(this)" method="post">
+        <div>
+            <label>Name:</label>
+            <input id="name" name="name" type="text" />
+        </div>
+        <div>
+            <label>Email:</label>
+            <input id="email" name="email" type="email" />
+        </div>
+        <div class="g-recaptcha" data-sitekey="6LffpPEgAAAAAOibceQnHN6enOfj99F7JaFrAACw"></div>
+        <div>
+            <input id="submit" type="submit" />
+        </div>
+    </form>
     </>
   );
 }
